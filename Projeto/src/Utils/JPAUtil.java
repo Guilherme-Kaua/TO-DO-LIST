6ace2e409265e499c4f629ec88f129fd1bc83e06
guilherme <guilherme.kaua.matos@gmail.com>
2025-10-14 16:48:45 -0300

    JPAUtil funcionando

--- Projeto/src/Utils/JPAUtil.java
+++ Projeto/src/Utils/JPAUtil.java
@@ -9,13 +9,12 @@ public class JPAUtil {
     // 1. A Factory é 'private', 'static' e 'final'.
     //    - static: para que seja única para toda a aplicação.
     //    - final: para que não possa ser alterada depois de inicializada.
-    private static final EntityManagerFactory FACTORY =
-            Persistence.createEntityManagerFactory("loja"); // 2. O nome aqui TEM de ser o mesmo do seu persistence.xml
+    private static final EntityManagerFactory FACTORY = Persistence.createEntityManagerFactory("jpa-pu");
 
     /**
-     * Este é o método que o resto da aplicação vai chamar.
-     * Cada vez que é chamado, ele cria um novo EntityManager.
-     * @return um novo EntityManager conectado à base de dados.
+     * Este é o metodo que o resto da aplicação vai chamar
+     * Cada vez que é chamado, ele cria um novo EntityManager
+     * @return um novo EntityManager conectado à base de dados
      */
     public static EntityManager getEntityManager() {
         return FACTORY.createEntityManager();
